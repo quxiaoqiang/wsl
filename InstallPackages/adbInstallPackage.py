b8ef362d08863181a381c9c693103b93d6f05d2f
xiaoqiang <confidenceqxq@163.com>
2018-01-09 19:06:50 +0800

    commit

--- InstallPackages/adbInstallPackage.py
+++ InstallPackages/adbInstallPackage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 
-import commands
+import commands,sys
+from testLogging.log import *
 
 #print commands.getstatusoutput('ls ..')
 #print commands.mkarg()
@@ -11,7 +12,6 @@ import commands
 def get_serial_numbers():
     devices = commands.getoutput('adb devices')  # 获取已连接的设备信息
     dev = devices.replace("\n", " ").replace("\t", " ").split(" ")  # 去掉\n和\t，并以空格分割字符串为list
-
     serial_numbers = []  # 取分割后生成的list中的设备serial_number保存
     for i in range(4, len(dev)):
         if dev[i] != 'device' and dev[i] != '':
@@ -21,17 +21,25 @@ def get_serial_numbers():
 def install_apks():
     # 安装apk到已连接的设备:"adb -s serial_numbers install -r apk_path"
     serial_numbers = get_serial_numbers()
+    if len(serial_numbers) ==0:
+        log().warning('没有检测到已连接的设备，请重新连接设备')
+        sys.exit()
+    else:
+        log().info('已连接的设备：%s' %serial_numbers)
+
     # 获取apk_path
     apk_path = raw_input("请输入apk_path:").split(" ")
-    for i in serial_numbers:
-        for k in apk_path:
-            if k != '':
-                aa = "adb -s %s install -r %s" % (i, k)
-                print aa
-                bb = commands.getoutput(aa)
-                print bb
 
-
+    #安装apk
+    for apk in apk_path:
+        for num in serial_numbers:
+            if apk != '':
+                log().info(num)
+                log().info(apk)
+                comm = "adb -s %s install -r %s" %(num,apk)
+                out = commands.getoutput(comm)
+                log().info(out)
+    return
 if __name__ == "__main__":
     install_apks()
 
---
+++ testLogging/log.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+import logging, sys
+
+'''
+1.创建logger，定义输入级别
+2.创建handler，用于写入日志文件
+3.创建handler，用于输出到控制台
+4.定义handler的输出格式
+5.将logger添加到handler里面
+'''
+def log():
+    # 指定日志的最低输出级别，默认为WARN级别
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)  # log等级的总开关
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+
+    # 文件日志
+    logfile = './test.log'
+    file_handler = logging.FileHandler(logfile)
+    file_handler.setLevel(logging.DEBUG)
+
+    # 控制台日志
+    console_handler = logging.StreamHandler()
+    file_handler.setLevel(logging.DEBUG)
+
+    # 设置日志输出格式
+    console_handler.formatter = formatter  # 也可以直接给formatter赋值
+    file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
+
+    # 为logger添加的日志处理器
+    logger.addHandler(file_handler)
+    #logger.addHandler(console_handler)
+    return logger
+
+
+
+if __name__ == '__main__':
+    # 输出不同级别的log
+    log().debug('this is debug info')
+    log().info('this is information')
+    log().warn('this is warning message')
+    log().error('this is error message')
+    log().fatal('this is fatal message, it is same as logger.critical')
+    log().critical('this is critical message')
+
+
+
